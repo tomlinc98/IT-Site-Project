67404da28f76e15e9959825178d926d722311f66
Chris <tomlinc98@gmail.com>
2022-12-06 14:41:31 +0000

    added urls for module page

--- IT_Site/base/urls.py
+++ IT_Site/base/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('about', views.about, name="about"),
     path('all_courses', views.all_courses, name="all_courses"),
     path('course/<str:pk>/', views.course, name="course"),
+    path('module/<str:pk>/', views.module, name="module"),
     path('faq', views.faq, name="faq"),
 ]
