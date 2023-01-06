e57383c204c2628cff0ea126206b9dc5c2b3dc11
Chris <tomlinc98@gmail.com>
2023-01-06 14:43:11 +0000

    added model for announcements

--- IT_Site/base/models.py
+++ IT_Site/base/models.py
@@ -36,3 +36,12 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    posted = models.DateTimeField(auto_now=True)  # last update
+
+    def __str__(self):
+        return self.title
